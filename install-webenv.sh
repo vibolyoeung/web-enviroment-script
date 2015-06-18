fe3fee2be2be980f239e63ad812fe68620578ce6
Vibol Yoeung <vyoeung1@we-bridge.net>
2015-06-18 09:53:29 +0700

    [ADD] File install php apache and mysql script

---
+++ install-webenv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+STAMP="/etc/apache2/.environment"
+if [ ! -f $STAMP ]; then
+  export DEBIAN_FRONTEND="noninteractive" ; set -e -x
+
+  APT="apt-get -y -o DPkg::Options::=--force-confdef -o DPkg::Options::=--force-confold -o APT::Get::force-yes=true"
+
+  #Avoid a few common cases of dependency version drift
+  sudo $APT update
+  #Install apache
+  sudo $APT install apache2
+
+  #Install mysql
+  sudo $APT install mysql-server libapache2-mod-auth-mysql php5-mysql
+
+  sudo mysql_install_db
+  sudo /usr/bin/mysql_secure_installation
+  sudo $APT install php5 libapache2-mod-php5 php5-mcrypt
+  apt-cache search php5-
+  sudo service apache2 restart
+  sudo touch $STAMP
+fi
